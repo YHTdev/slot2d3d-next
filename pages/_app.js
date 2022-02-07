62a169683fdf484e0f1fbf5bfa010989cdfa7f53
myomaung5255225 <myomaung5255225@gmail.com>
2022-02-08 01:11:36 +0630

    update_home

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,23 @@
 import Layout from "../components/layout";
 import { SessionProvider, signIn, useSession } from "next-auth/react";
 import "../styles/globals.scss";
-
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useCallback, useEffect } from "react";
 function MyApp({ Component, pageProps }) {
+  const cbInit = useCallback(() => {
+    AOS.init({
+      easing: "ease-out-cubic",
+      once: true,
+      offset: 50,
+    });
+  }, []);
+
+  useEffect(() => {
+    cbInit()
+  }, [cbInit]);
+  
+  
   return (
     <SessionProvider>
       {Component.auth ? (
