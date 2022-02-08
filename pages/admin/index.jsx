0fe0e6b53fd052f2d56117f260f9fef18b6d471b
ARKAR-PHYO <arkarphyo.edu@gmail.com>
2022-02-09 00:24:12 +0630

    start admin dashboard

---
+++ pages/admin/index.jsx
@@ -0,0 +1,11 @@
+import Layout from "../../components/layout";
+
+const Dashboard = () => {
+  return (
+    <Layout>
+      <h1>Admin Dashboard</h1>
+    </Layout>
+  );
+};
+
+export default Dashboard;
