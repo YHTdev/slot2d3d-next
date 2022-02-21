8b65a50acbf6c1d4ecb36bab125ed2e645aa38b3
myomaung5255225 <myomaung5255225@gmail.com>
2022-02-21 23:15:31 +0630

    updated_limit

---
+++ pages/admin/_middleware.js
@@ -0,0 +1,29 @@
+
+
+import { verifyToken } from '../../lib/JWT'
+import { jsonResponse } from '../../lib/responseHelper'
+
+export default async function middleware (req) {
+    
+  try {
+    const accessToken = req.cookies['USER_TOKEN']
+
+    if (accessToken) {
+      const user = verifyToken(accessToken)
+
+      if (user) {
+        if (user.role === 'SYS_ADMIN' || user.role === 'ADMIN') {
+        } else {
+          return jsonResponse(401, { message: 'Unauthenticated' })
+        }
+      } else {
+        return jsonResponse(401, { message: 'Unauthenticated' })
+      }
+    } else {
+      return jsonResponse(401, { message: 'Unauthenticated' })
+    }
+  } catch (error) {
+      console.log(error)
+    return jsonResponse(500, { message: 'System Error' })
+  }
+}
